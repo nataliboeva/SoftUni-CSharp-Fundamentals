e11bc9018566b8828b98c9b5ee546a446a36d1a0
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-01-31 17:29:49 +0200

    Add files via upload

---
+++ Fundamentals/BasicSyntax/10. Multiplication Table.cs
@@ -0,0 +1,16 @@
+﻿namespace _10._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+                i++;
+            }
+        }
+    }
+}

c32fbf46334556a4ee4805330cc06b72c0261ece
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-01-31 17:35:56 +0200

    Add files via upload

---
+++ Fundamentals/BasicSyntaxExcercise/11. Orders.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int orders = int.Parse(Console.ReadLine());
+            double total = 0;
+            for (int i = 0; i < orders; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+                double price = (days * capsulesCount) * pricePerCapsule;
+                total += price;
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}

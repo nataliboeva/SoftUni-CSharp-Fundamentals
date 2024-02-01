ac3daaf08eb54ffac5506435bb32f356cdd01fae
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-01 21:32:26 +0200

    Add files via upload

---
+++ Fundamentals/DataTypesAndVariablesExercise/02. Sum Digits.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double sum = 0;
+            
+            while (number > 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Fundamentals/DataTypesAndVariablesExercise/03. Elevator.cs
@@ -0,0 +1,14 @@
+﻿namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            double courses = (int)Math.Ceiling((double)numberOfPeople / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}

216dfffc2651b1a5185ce716facbc1f922be4daf
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-01 21:30:41 +0200

    Add files via upload

---
+++ Fundamentals/DataTypesAndVariables/06. Reversed Chars.cs
@@ -0,0 +1,14 @@
+﻿namespace Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            Console.Write(thirdLetter + " " + secondLetter + " " + firstLetter);
+        }
+    }
+}

3f48db8ae8a4a9f0a4ecb56f5b46431d9ecd114c
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-01 21:27:57 +0200

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/01. Integer Operations.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            double result = num2 += num1;
+            result = Math.Round(result/num3);
+            result = result*num4;
+
+            Console.WriteLine(result);
+        }
+    }
+}

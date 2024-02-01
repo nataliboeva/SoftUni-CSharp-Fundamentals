1a9e8bce91d1f8d4021ef22523b17bcb2a6696bc
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-01 18:55:50 +0200

    Add files via upload

---
+++ Fundamentals/Data Types and Variables/05. Special Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) 
+            {
+                int currentNum = i;
+                int sum = 0;
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+                bool special = ((sum == 5) || (sum == 7) || (sum == 11));
+
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Data Types and Variables/08. Town Info.cs
@@ -0,0 +1,14 @@
+﻿namespace Town_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            decimal population = decimal.Parse(Console.ReadLine());
+            double area = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}

25234a35c3fc3149c1cfe27aabb641cb077604fe
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-03 13:47:49 +0200

    Add files via upload

---
+++ Fundamentals/DataTypesAndVariablesExercise/10. Poke Mon.cs
@@ -0,0 +1,26 @@
+﻿namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustion = int.Parse(Console.ReadLine());
+            int target = 0;
+            int half = pokePower / 2;
+            while (pokePower > distance) 
+            {
+                pokePower -= distance;
+                target++;
+
+                if (pokePower == half && exhaustion != 0) 
+                {
+                    pokePower = pokePower / exhaustion;
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(target);
+        }
+    }
+}

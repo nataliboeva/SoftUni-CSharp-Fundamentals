7883ff1cefae4da45191e892ffd7ad60e4d6b2fc
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-09 19:17:15 +0200

    Add files via upload

---
+++ Fundamentals/ArraysLab/07. Equal Arrays.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] second = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                else
+                {
+                    sum += first[i];
+                }
+
+            }
+                    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}

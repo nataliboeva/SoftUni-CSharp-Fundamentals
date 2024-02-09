2e6390ef26392749546a7f8a4df046add526ca2c
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-09 18:47:37 +0200

    Add files via upload

---
+++ Fundamentals/ArraysLab/05. Sum Even Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+
+namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int check = input[i];
+                if (check % 2 == 0)
+                {
+                    sum += input[i];
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

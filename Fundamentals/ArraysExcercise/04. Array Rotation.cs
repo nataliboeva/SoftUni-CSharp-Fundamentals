46d001dc02fcbd35631bbbaa115c83899bf5007f
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-10 22:07:49 +0200

    Add files via upload

---
+++ Fundamentals/ArraysExcercise/04. Array Rotation.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            while (rotations > 0)
+            {
+                int first = numbers[0];
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    numbers[i] = numbers[i + 1];
+                }
+                numbers[numbers.Length - 1] = first;
+                rotations--;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

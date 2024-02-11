be287e98cf0d1186455469b2fe964d1b0da53d9c
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-11 16:03:11 +0200

    Add files via upload

---
+++ Fundamentals/ArraysExcercise/06. Equal Sum.cs
@@ -0,0 +1,50 @@
+﻿namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers.Length == 1)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+                    sumLeft = 0;
+
+                for (int left = i; left > 0; left--)
+                {
+                     int next = left - 1;
+                     if(left > 0)
+                     {
+                        sumLeft += numbers[next];
+                     }
+                }
+                    sumRight = 0; 
+
+                for (int right = i; right < numbers.Length; right++)
+                {
+                    int next = right + 1;
+
+                    if (right < numbers.Length - 1)
+                    {
+                       sumRight += numbers[next];
+                    } 
+                }
+
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+
+            }
+            Console.WriteLine("no");
+        }
+    }
+}

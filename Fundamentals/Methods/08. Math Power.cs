6e0cb752c78e4de193ce6662354f5bbc7e93e593
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-12 19:50:13 +0200

    Add files via upload

---
+++ Fundamentals/Methods/08. Math Power.cs
@@ -0,0 +1,17 @@
+﻿namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static double Pow(double num, double power)
+        {
+            return Math.Pow(num,power);
+        }
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double pow = Pow(number, power);
+            Console.WriteLine(pow);
+        }
+    }
+}

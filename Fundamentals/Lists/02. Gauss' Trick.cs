56f483b5e1be3403c8d6d4ea7d85dd0b0b1247c2
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-15 18:49:38 +0200

    Add files via upload

---
+++ Fundamentals/Lists/02. Gauss' Trick.cs
@@ -0,0 +1,22 @@
+﻿namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                int first = numbers[i];
+                int last = numbers[numbers.Count - i - 1];
+                result.Add(first + last);
+            }
+            if(numbers.Count % 2 == 1)
+            {
+                result.Add(numbers[numbers.Count/2]);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}

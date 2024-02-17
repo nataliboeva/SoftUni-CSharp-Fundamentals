1266ed72dd6ac328ef2cadcd407efe6581c73e83
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-17 23:49:15 +0200

    Add files via upload

---
+++ Fundamentals/Exam Preparation/Problem 1 – Black Flag.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem_1___Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int daily = int.Parse(Console.ReadLine());
+            double expected = double.Parse(Console.ReadLine());
+
+            double total = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                total += daily;
+
+                if(i % 3 == 0)
+                {
+                    total += daily * 0.5;
+                }
+                if(i % 5 == 0)
+                {
+                    total *=  0.7;
+                }
+            }
+
+            if(total>= expected)
+            {
+                Console.WriteLine($"Ahoy! {total:f2} plunder gained.")
+;           }
+            else
+            {
+                var percentage = total / expected * 100; 
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+        }
+    }
+}

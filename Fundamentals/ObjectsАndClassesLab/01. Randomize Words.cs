2f80ca77865a1065947007597721c307f45585b0
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-02-21 22:35:34 +0200

    Add files via upload

---
+++ Fundamentals/ObjectsАndClassesLab/01. Randomize Words.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Random random = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(words.Length);
+                string word = words[i];
+                string randomIndexWord = words[randomIndex];
+
+                words[i] = randomIndexWord;
+                words[randomIndex] = word;
+
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+
+    }
+}

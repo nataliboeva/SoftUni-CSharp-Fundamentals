e0a5d955320dffa72115110c616f51d403906417
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-07 20:03:51 +0200

    Add files via upload

---
+++ 01. Basic Syntax/08. Divisible by 3.cs
@@ -0,0 +1,13 @@
+﻿namespace _08._Divisible_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

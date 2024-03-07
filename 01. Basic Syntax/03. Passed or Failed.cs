2204b4aae0878bd40e4dd175e812a658802ba4df
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-07 20:02:43 +0200

    Add files via upload

---
+++ 01. Basic Syntax/03. Passed or Failed.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Passed_or_Failed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}

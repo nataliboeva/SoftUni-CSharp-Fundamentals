6f6de78877d6be512c755f46748308017e3ebe9d
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-07 20:03:24 +0200

    Add files via upload

---
+++ 01. Basic Syntax/06. Foreign Languages.cs
@@ -0,0 +1,25 @@
+﻿namespace _06._Foreign_Languages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string language = Console.ReadLine();
+            switch (language)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}

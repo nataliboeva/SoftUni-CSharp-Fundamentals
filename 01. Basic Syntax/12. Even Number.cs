cd819ffe06ab3137bc165d680d6cc12ce13ce56a
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-07 20:04:48 +0200

    Add files via upload

---
+++ 01. Basic Syntax/12. Even Number.cs
@@ -0,0 +1,23 @@
+﻿namespace _12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = int.Parse(Console.ReadLine());
+            if (i <= 10)
+            {
+                while (i <= 10)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}

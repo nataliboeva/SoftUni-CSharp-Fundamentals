13f319d6164342282a9e30a7b7923d808f0351d8
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-08 09:24:35 +0200

    Add files via upload

---
+++ 04. Data Types and Variables  Excercise/06. Triples of Latin Letters.cs
@@ -0,0 +1,23 @@
+﻿namespace _06._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int startChar = 97;
+            int endChar = startChar + count;
+
+            for (int i = startChar; i < endChar; i++)
+            {
+                for (int j = startChar; j < endChar; j++)
+                {
+                    for (int k = startChar; k < endChar; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }
+        }
+    }
+}

ae39f36a73bb3ae7a365821a7e0dbdc376eccc3c
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-08 09:23:50 +0200

    Add files via upload

---
+++ 04. Data Types and Variables  Excercise/05. Print Part Of ASCII Table.cs
@@ -0,0 +1,16 @@
+﻿namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int from = int.Parse(Console.ReadLine());
+            int to = int.Parse(Console.ReadLine());
+            for (int i = from; i <= to; i++)
+            {
+                char symbol = (char)i;
+                Console.Write(symbol + " ");
+            }
+        }
+    }
+}

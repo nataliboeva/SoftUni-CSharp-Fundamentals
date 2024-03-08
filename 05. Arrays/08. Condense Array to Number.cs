7d8394b981a38190f7e8a822502fe58e3a3b34de
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-08 09:29:12 +0200

    Add files via upload

---
+++ 05. Arrays/08. Condense Array to Number.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers =  Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine(numbers[0]);
+                return;
+            }
+
+            while (numbers.Length > 1)
+            {
+                int[] condensed = new int [numbers.Length - 1];
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+                }
+
+                numbers = condensed;
+            }
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}

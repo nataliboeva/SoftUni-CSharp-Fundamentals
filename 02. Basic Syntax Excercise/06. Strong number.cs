05e8d6c74583bec37d0433a550fef79d0bde503d
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-08 08:54:50 +0200

    Add files via upload

---
+++ 02. Basic Syntax Excercise/06. Strong number.cs
@@ -0,0 +1,39 @@
+﻿namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int numCopy = num;
+            int sum = 0;
+
+            while (numCopy > 0)
+            {
+                //num = 145
+                int digit = numCopy % 10; //5
+                numCopy = numCopy / 10;
+
+                int factorial = 1;
+                for (int i = 1; i <= digit; i++)
+                {
+                    factorial *= i;
+                }
+
+                sum += factorial;
+            }
+
+            //if (num == sum) 
+            // {
+            //     Console.WriteLine("yes");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("no");
+            // }
+            string steli = num == sum ? "yes" : "no";
+            Console.WriteLine(steli);
+        }
+    }
+}

ac438257f9beb94992366d418c9fc41e413c488e
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-08 09:27:49 +0200

    Add files via upload

---
+++ 05. Arrays/03. Rounding Numbers.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int round = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine(numbers[i] + " => " + round);
+            }
+            
+        }
+    }
+}

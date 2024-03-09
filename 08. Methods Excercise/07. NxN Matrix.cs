9feeac51a59f665c5f491ee54bb6ed4aa7c1727c
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 19:01:29 +0200

    Add files via upload

---
+++ 08. Methods Excercise/07. NxN Matrix.cs
@@ -0,0 +1,23 @@
+﻿namespace NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Matrix(n);
+        }
+        static void Matrix(int num)
+        {
+            for (int rol = 0; rol < num; rol++)
+            {
+                Console.Write(num + " ");
+                for (int col = 1; col < num; col++)
+                {
+                    Console.Write( num + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

df94db1ce89a95eec706789f6b73db83334a704b
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 18:53:59 +0200

    Add files via upload

---
+++ 06. Arrays Excercise/02. Common Elements.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine().Split();
+            string[] second = Console.ReadLine().Split();
+            string[] check = new string[first.Length + second.Length];
+
+            for (int i = 0; i < second.Length; i++)
+            {
+                for (int j = 0; j < first.Length; j++)
+                {
+                    if (second[i] == first[j])
+                    {
+                        check[i] = second[i];
+                        Console.Write(check[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}

d1f859022fe20091be8063c7f8c8765bf798ef95
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 19:04:33 +0200

    Add files via upload

---
+++ 09. List/01. Sum Adjacent Equal Numbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            double current;
+            double next;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                current = numbers[i];
+                next = numbers[i + 1];
+                if(current == next)
+                {
+                    numbers[i] = current + next;
+                    numbers.Remove(next);
+                    i = - 1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

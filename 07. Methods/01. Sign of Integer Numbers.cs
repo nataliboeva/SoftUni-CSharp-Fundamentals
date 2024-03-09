95884cf280574c36ca89d7637e8fa8f36a6abc9d
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 18:56:37 +0200

    Add files via upload

---
+++ 07. Methods/01. Sign of Integer Numbers.cs
@@ -0,0 +1,26 @@
+﻿namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SignOfIntegerNumbers(n);
+        }
+        static void SignOfIntegerNumbers(int number)
+        {
+            if(number  == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+        }
+    }
+}

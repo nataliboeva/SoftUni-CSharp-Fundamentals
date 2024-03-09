3681019c82cf3ad42a0c8258c7b471a6fab93d24
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 19:01:00 +0200

    Add files via upload

---
+++ 08. Methods Excercise/05. Add and Subtract.cs
@@ -0,0 +1,19 @@
+﻿namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int result = AddAndSubtract(first, second, third);
+            Console.WriteLine(result);
+        }
+        static int AddAndSubtract(int a, int b, int c)
+        {
+            int result = a + b;
+            return result - c;
+        }
+    }
+}

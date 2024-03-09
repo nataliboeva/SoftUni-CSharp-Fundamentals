f409c3c3ac5da3e0da1b228ac2e9a5aa19996319
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 18:54:42 +0200

    Add files via upload

---
+++ 06. Arrays Excercise/05. Top Integers.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isBigger = true;
+                for (int j = i + 1 ; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if(isBigger)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}

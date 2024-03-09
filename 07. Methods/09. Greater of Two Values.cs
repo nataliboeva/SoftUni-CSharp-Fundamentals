b892ff24ca25cd4cee4ebc8df4f12ff7ae853b93
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-09 18:58:33 +0200

    Add files via upload

---
+++ 07. Methods/09. Greater of Two Values.cs
@@ -0,0 +1,55 @@
+﻿namespace _09._Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void GetMax(string type)
+        {
+            if (type == "int")
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                if (a > b)
+                {
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(b);
+                }
+            }
+            else if (type == "char")
+            {
+                char one = char.Parse(Console.ReadLine());
+                char two = char.Parse(Console.ReadLine());
+                if (one > two)
+                {
+                    Console.WriteLine(one);
+                }
+                else
+                {
+                    Console.WriteLine(two);
+                }
+
+            }
+            else if(type == "string")
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+                int command = first.CompareTo(second);
+                if(command > 0)
+                {
+                    Console.WriteLine(first);
+                }
+                else
+                {
+                    Console.WriteLine(second);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            GetMax(input);
+        }
+    }
+}

1d25be36bd68caf5b0ef6dad259251c9a39b4035
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-10 10:43:43 +0200

    Add files via upload

---
+++ 11. Regular Mid Exam/Exam Preparation/Problem 3 – Numbers (EasierSolution).cs
@@ -0,0 +1,15 @@
+﻿namespace Problem_3___Numbers__EasierSolution_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            double average = numbers.Average();
+            List<int> topFive = numbers.FindAll(n => n > average)
+                .OrderByDescending(n => n).Take(5).ToList();
+
+            Console.WriteLine(topFive.Count == 0 ? "No" : string.Join(" ", topFive));
+        }
+    }
+}

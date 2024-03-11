bdb219db232cb356cd3964ba79b58e0671b12eaf
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-11 21:35:22 +0200

    Add files via upload

---
+++ 14. Associative Arrays/04. Word Filter.cs
@@ -0,0 +1,15 @@
+﻿namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}

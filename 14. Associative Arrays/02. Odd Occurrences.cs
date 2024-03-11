758cdba234fcd3af32f4294e42bdc483c7a8c708
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-11 21:34:36 +0200

    Add files via upload

---
+++ 14. Associative Arrays/02. Odd Occurrences.cs
@@ -0,0 +1,29 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Select(x => x.ToLower()).ToArray();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach (string item in words)
+            {
+                if (counts.ContainsKey(item))
+                {
+                    counts[item]++;
+                }
+                else
+                {
+                    counts.Add(item, 1);
+                }
+            }
+            foreach (var count in counts)
+            {
+                if(count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}

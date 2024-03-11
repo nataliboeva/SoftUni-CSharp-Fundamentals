6f46a42bb21ddb782d2fa6ac0c0396bd5eac4891
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-11 21:34:59 +0200

    Add files via upload

---
+++ 14. Associative Arrays/03. Word Synonyms.cs
@@ -0,0 +1,27 @@
+﻿namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string key = Console.ReadLine();
+                string value = Console.ReadLine();
+                if (!synonyms.ContainsKey(key))
+                {
+                    synonyms.Add(key, new List<string>()); 
+                }
+                synonyms[key].Add(value);
+
+            }
+            foreach(var synonym in synonyms)
+            {
+                Console.WriteLine(synonym.Key + " - " + string.Join(", ", synonym.Value));
+            }
+            
+        }
+    }
+}

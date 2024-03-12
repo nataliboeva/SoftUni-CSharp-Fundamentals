7843a116d9a8a441fa57eb53c90f2673216394de
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-12 21:22:08 +0200

    Add files via upload

---
+++ 15. Associative Arrays Exercise/02. A Miner Task.cs
@@ -0,0 +1,27 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            while((input = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!map.ContainsKey(input))
+                {
+                    map.Add(input, quantity);
+                }
+                else
+                {
+                    map[input] += quantity;
+                }
+            }
+            foreach (var kvp in map)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+        }
+    }
+}

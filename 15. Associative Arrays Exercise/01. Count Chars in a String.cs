22fffb337cda721bbaa0699352df0ac5bdbaae9f
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-12 21:11:34 +0200

    Add files via upload

---
+++ 15. Associative Arrays Exercise/01. Count Chars in a String.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+                if(character == ' ')
+                {
+                    continue;
+                }
+                if (!map.ContainsKey(character))
+                {
+                    map.Add(character, 0);
+                }
+                map[character]++;
+            }
+            foreach (var c in map)
+            {
+                Console.WriteLine(c.Key + " -> " + c.Value);
+            }
+        }
+    }
+}

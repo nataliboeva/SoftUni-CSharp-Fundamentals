3a4c4a90a96c199711b5f9ef765892753feedbfb
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-14 21:55:08 +0200

    Add files via upload

---
+++ 15. Associative Arrays Exercise/05. Courses.cs
@@ -0,0 +1,46 @@
+﻿namespace _05._Courses
+{
+    internal class Program
+    {
+        class Courses
+        {
+            public string Name { get; set; }
+            public List<string> StudentNames { get; set; }
+            public Courses(string name)
+            {
+                Name = name;
+                StudentNames = new List<string>();
+            }
+            public override string ToString()
+            {
+                string result = $"{Name}: {StudentNames.Count}\n";
+                for (int i = 0; i < StudentNames.Count; i++)
+                {
+                    result += $"-- {StudentNames[i]}\n";
+                }
+                return result.Trim();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string input;
+            Dictionary<string, Courses> registredStu = new Dictionary<string, Courses>();
+            while((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split(" : ").ToArray();
+                string course = command[0];
+                string student = command[1];
+                if (!registredStu.ContainsKey(course))
+                {
+                    Courses courses = new Courses(course);
+                    registredStu.Add(course, courses);
+                }
+                registredStu[course].StudentNames.Add(student);
+            }
+            foreach (Courses course in registredStu.Values)
+            {
+                Console.WriteLine(course);
+            }
+        }
+    }
+}

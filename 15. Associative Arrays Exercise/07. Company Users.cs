5818d4835148b190f7ef3e5560c4e5af765e4917
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-15 22:35:53 +0200

    Add files via upload

---
+++ 15. Associative Arrays Exercise/07. Company Users.cs
@@ -0,0 +1,34 @@
+﻿namespace _07._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Dictionary<string, List<string>> employees = new Dictionary<string, List<string>>(); 
+            while((input = Console.ReadLine()) != "End")
+            {
+                string[] arr = input.Split(" -> ").ToArray();
+                string company = arr[0];
+                string id = arr[1];
+
+                if(!employees.ContainsKey(company))
+                {
+                    employees.Add(company, new List<string>());
+                }
+                if (!employees[company].Contains(id))
+                {
+                    employees[company].Add(id);
+                }
+            }
+            foreach(var  employee in employees)
+            {
+                Console.WriteLine(employee.Key);
+                foreach(var id in  employee.Value)
+                {
+                    Console.WriteLine("-- " + id);
+                }
+            }
+        }
+    }
+}

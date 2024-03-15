e130e930c54ccb90d6d30c01fe892681b54ccdcb
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-15 22:21:50 +0200

    Add files via upload

---
+++ 15. Associative Arrays Exercise/06. Student Academy.cs
@@ -0,0 +1,30 @@
+﻿namespace _06._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if(!students.ContainsKey(student))
+                {
+                    students.Add(student, new List<double>());
+                }
+                students[student].Add(grade);            
+            }
+            foreach(var info in students)
+            {
+                if(info.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{info.Key} -> {info.Value.Average():f2}");
+                }
+                
+            }
+        }
+    }
+}

b05ad2b58360d66b45bd80e5ab5fce5153be8d33
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-17 23:48:40 +0200

    Add files via upload

---
+++ 16. Text Processing/05. Digits, Letters and Other.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string letters = string.Empty;
+            string numbers = string.Empty;
+            string others = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char current = text[i];
+                if (char.IsLetter(current))
+                {
+                    letters += current;
+                }
+                else if(char.IsDigit(current))
+                {
+                    numbers += current;
+                }
+                else
+                {
+                    others += current;
+                }
+            }
+
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}

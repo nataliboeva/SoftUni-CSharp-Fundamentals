e861f72467d95ed02bfdfd43343110537ced0ef5
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-17 23:22:26 +0200

    Add files via upload

---
+++ 16. Text Processing/04. Text Filter.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banned = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach(var ban in banned)
+            {
+                text = text.Replace(ban, new string('*', ban.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}

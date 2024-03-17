0cb099a2b8bf775037cf708d373adfb1bf065285
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-17 22:50:49 +0200

    Add files via upload

---
+++ 16. Text Processing/01. Reverse Strings.cs
@@ -0,0 +1,15 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while((input = Console.ReadLine()) != "end")
+            {
+                string reversed = new string(input.Reverse().ToArray());
+                Console.WriteLine(input + " = " + reversed);
+            }
+        }
+    }
+}

91817d75f923239048eff9114072ca7fa4b2c848
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-17 22:51:06 +0200

    Add files via upload

---
+++ 16. Text Processing/02. Repeat Strings.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.Write(word);
+                }
+            }    
+        }
+    }
+}

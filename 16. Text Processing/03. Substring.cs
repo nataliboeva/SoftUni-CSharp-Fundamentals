359979eeb9231d329377e99afae60dbd3332a266
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-17 23:11:26 +0200

    Add files via upload

---
+++ 16. Text Processing/03. Substring.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            string word = Console.ReadLine();
+
+            while(word.Contains(filter))
+            {
+                int remove = word.IndexOf(filter);
+                word = word.Remove(remove, filter.Length);
+            }
+            Console.WriteLine(word);
+        }
+    }
+}

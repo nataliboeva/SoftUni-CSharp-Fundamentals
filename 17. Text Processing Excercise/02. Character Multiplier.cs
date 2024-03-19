e494f8a3bdc5f696d3ae73b5c996edfb5b4a74a9
Natali Boeva <121763213+nnattiee@users.noreply.github.com>
2024-03-19 22:01:40 +0200

    Add files via upload

---
+++ 17. Text Processing Excercise/02. Character Multiplier.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int max = Math.Max(input[0].Length, input[1].Length);
+            int result = 0;
+            for (int i = 0; i < max; i++)
+            {
+                int multiplier1 = 1, multiplier2 = 1;
+                if(i < input[0].Length) multiplier1 = input[0][i];
+                if(i < input[1].Length) multiplier2 = input[1][i];
+                result += multiplier1 * multiplier2;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}

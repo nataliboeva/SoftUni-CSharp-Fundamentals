687be5201464296a64c83c8dc0f13235a7b32f4b
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-21 21:52:14 +0200

    Add files via upload

---
+++ 17. Text Processing Excercise/03. Extract File.cs
@@ -0,0 +1,20 @@
+﻿namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int lastIndex = input.LastIndexOf("\\");
+            string fullFileName = input.Substring(lastIndex + 1);
+
+            int lastDot = fullFileName.LastIndexOf('.');
+            string name = fullFileName.Substring(0, lastDot);
+            string extension = fullFileName.Substring(lastDot + 1);
+
+            Console.WriteLine("File name: " + name);
+            Console.WriteLine("File extension: " + extension);
+        }
+    }
+}

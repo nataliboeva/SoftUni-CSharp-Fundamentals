faae968aaca775aeb6b9ee00d4841a89e8b5b4aa
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-23 21:56:07 +0200

    Add files via upload

---
+++ 17. Text Processing Excercise/04. Caesar Cipher.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char substitute = (char)(text[i] + 3);
+                sb.Append(substitute);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

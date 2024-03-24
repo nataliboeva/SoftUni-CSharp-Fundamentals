62856919ada71f8e51463c31d9488c362d7507c7
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-24 14:14:34 +0200

    Add files via upload

---
+++ 17. Text Processing Excercise/07. String Explosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder(capacity: line.Length);
+            int strength = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '>')
+                {
+                    strength += line[i + 1] - '0';
+                    sb.Append(line[i]);
+                }
+                else if(strength > 0)
+                {
+                    strength--;
+                }
+                else
+                {
+                    sb.Append(line[i]);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

5621dc55ca9c19d7500f693b5c7375c87e506662
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-24 14:39:57 +0200

    Add files via upload

---
+++ 17. Text Processing Excercise/08. Letters Change Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace _08._Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            decimal result = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                string current = line[i];
+                char before = current[0];
+                char after = current[current.Length - 1];
+                decimal number = int.Parse(current.Substring(1, current.Length - 2));
+                decimal score = 0;
+                if (char.IsUpper(before)) score += number / (before - 'A' + 1);
+                else score += number * (before - 'a' + 1);
+
+                if (char.IsUpper(after)) score -= after - 'A' + 1;
+                else score += after - 'a' + 1;
+
+                result += score;
+            }
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}

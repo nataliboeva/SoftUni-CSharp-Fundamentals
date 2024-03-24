b740c84ab536e965aa30749be2957b3e7c714af8
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-24 14:14:19 +0200

    Add files via upload

---
+++ 17. Text Processing Excercise/06. Replace Repeating Chars.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(i == 0 || text[i - 1] != text[i])
+                {
+                    sb.Append(text[i]);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

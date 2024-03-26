1bc8cff755629a58c581ab4f22f56c8f4a5a14a5
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-26 22:03:53 +0200

    Add files via upload

---
+++ 18. Regular Expressions/03. Match Dates.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<date>\d{2})(?<seperator>\.|\-|\/)(?<month>[A-Z][a-z]{2})(\k<seperator>)(?<year>\d{4})";
+            string input = Console.ReadLine();
+
+            var matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["date"].Value}, Month: {match.Groups["month"].Value}, Year: {match.Groups["year"].Value}");
+            }
+        }
+    }
+}

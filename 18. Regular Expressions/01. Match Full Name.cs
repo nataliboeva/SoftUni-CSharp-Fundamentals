8565980eb3b16c970cd0063a6027f1126988d636
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-27 22:52:30 +0200

    Add files via upload

---
+++ 18. Regular Expressions/01. Match Full Name.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            string input = Console.ReadLine();
+
+            var match = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(" ", match.Select(x => x.Groups[0])));
+        }
+    }
+}

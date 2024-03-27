3851efd5deb9fa9c0be07affdcf66a1f5f2339b3
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-27 23:10:38 +0200

    Add files via upload

---
+++ 18. Regular Expressions/02. Match Phone Number.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}(?!\d)";
+            string input = Console.ReadLine();
+
+            var match = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", match.Select(x => x.Groups[0])));
+        }
+    }
+}

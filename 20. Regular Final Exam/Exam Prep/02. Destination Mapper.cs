404912e12e8abe43e398e0e8287fe7b97c36d13a
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-31 22:40:25 +0300

    Add files via upload

---
+++ 20. Regular Final Exam/Exam Prep/02. Destination Mapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|\/)(?<destination>[A-Z][A-Za-z]{2,})(\1)";
+            string input = Console.ReadLine();
+            List<string> list = new List<string>();
+            int sumLenght = 0;
+
+            foreach(Match match in Regex.Matches(input, pattern))
+            {
+                string result = match.Groups["destination"].Value;
+                list.Add(result);
+                sumLenght += result.Length;
+            }
+
+            Console.WriteLine("Destinations: " + string.Join(", ",list));
+            Console.WriteLine("Travel Points: " + sumLenght);
+        }
+    }
+}

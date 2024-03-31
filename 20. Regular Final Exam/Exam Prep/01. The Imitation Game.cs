de19c0381e5efdd63643ebd3f9096ecb8f263e73
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-03-31 22:38:19 +0300

    Add files via upload

---
+++ 20. Regular Final Exam/Exam Prep/01. The Imitation Game.cs
@@ -0,0 +1,36 @@
+﻿namespace Exam_Prep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine()) != "Decode")
+            {
+                string[] token = input.Split('|');
+                string command = token[0];
+                if (command == "Move")
+                {
+                    int numberOfLetters = int.Parse(token[1]);
+                    string keep = encryptedMessage.Substring(0, numberOfLetters);
+                    encryptedMessage = encryptedMessage.Remove(0, numberOfLetters);
+                    encryptedMessage += keep;
+                }
+                else if (command == "Insert")
+                {
+                    int index = int.Parse(token[1]);
+                    string value = token[2];
+                    encryptedMessage = encryptedMessage.Insert(index, value);
+                }
+                else if (command == "ChangeAll")
+                {
+                    string substring = token[1];
+                    string replacement = token[2];
+                    encryptedMessage = encryptedMessage.Replace(substring, replacement);
+                }
+            }
+            Console.WriteLine("The decrypted message is: " + encryptedMessage);
+        }
+    }
+}

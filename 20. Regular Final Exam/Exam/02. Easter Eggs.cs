688e26e95ca9d174fb60bde8138bef5050bee229
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-04-01 22:38:11 +0300

    Add files via upload

---
+++ 20. Regular Final Exam/Exam/02. Easter Eggs.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Easter_Eggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"[@|#]+(?<color>[a-z]{3,})[@|#]+[^a-z\d]*\/+(?<amount>\d+)\/+";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+
+                Console.WriteLine($"You found {match.Groups["amount"].Value} {match.Groups["color"].Value} eggs!");
+            }
+        }
+    }
+}
